5ceac115997d6ce09f0731bc8c6dbc6a2ec3f5aa
niknov80 <n.novozhilov80@gmail.com>
2023-10-13 12:15:07 +0300

    Добавлено: - отдельная команда на оптимизацию изображений - миксин image - миксин promo-video - watcher при review - плагин gulp-format-html, для компиляции html в красивом виде - команда для деплоя проекта на github
    Изменено:
    
    Устарело
    
    Удалено:
    
    Исправлено:
    
    Безопасность:

--- gulpfile.js
+++ gulpfile.js
@@ -25,6 +25,7 @@ import { otfToTtf, ttfToWoff, copyWoff } from './gulp/tasks/fonts.js';
 import { compileMainMinScripts, compileMainScripts, compileVendorScripts } from './gulp/tasks/scripts.js';
 import { validateMarkup } from "./gulp/tasks/w3c.js";
 import { lintBem } from "./gulp/tasks/bem.js";
+import {deployToGithub} from "./gulp/tasks/deploy.js";
 
 // Наблюдатель за изменениями в файлах
 function Watcher() {
@@ -42,9 +43,10 @@ const convertOtfToTtf = otfToTtf;
 const validateW3C = validateMarkup;
 const lintingBem = lintBem;
 const optimizeImage = images;
+const deploy = deployToGithub;
 
 const dev = gulp.series(clean, fontTask, pug, copyImage, svg, sprite, styles, compileMainScripts, compileVendorScripts, gulp.parallel(Watcher, server));
 const preview = gulp.series(clean, fontTask, pug, images, svg, sprite, styles, compileMainMinScripts, compileVendorScripts, gulp.parallel(Watcher, server));
 const build = gulp.series(clean, fontTask, pug, images, svg, sprite, styles, compileMainMinScripts, compileVendorScripts);
 
-export { dev, preview, build, convertOtfToTtf, validateW3C, lintingBem, optimizeImage }
+export { dev, preview, build, deploy, convertOtfToTtf, validateW3C, lintingBem, optimizeImage }
